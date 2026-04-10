d4af9722e4a87778129c65344e853107aa094c39
Christine Zhou <zhouchrs@amazon.com>
2026-04-09 23:15:14 -0700

    Force cache bust: version CSS, nuke all SW caches on activate

--- majiang/sw.js
+++ majiang/sw.js
@@ -1,6 +1,8 @@
-const CACHE = 'majiang-v2';
-const ASSETS = ['./', './index.html', './style.css', './tiles.js', './i18n.js', './sound.js', './game.js', './rogue.js'];
+const CACHE = 'majiang-v3';
 
-self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))));
+  return self.clients.claim();
+});
 self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
