97c864ad3f316879bdd5f4d4f0a01c7c242de1fb
Christine Zhou <zhouchrs@amazon.com>
2026-04-09 23:13:28 -0700

    Bigger tiles & fonts for elderly-friendly mobile, bust SW cache

--- majiang/sw.js
+++ majiang/sw.js
@@ -1,5 +1,6 @@
-const CACHE = 'majiang-v1';
+const CACHE = 'majiang-v2';
 const ASSETS = ['./', './index.html', './style.css', './tiles.js', './i18n.js', './sound.js', './game.js', './rogue.js'];
 
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
